d14a9d89dce951ea30c7f2f4c410cd1ecf63f2f9
heberGustavo <heber.gbarbosa@hotmail.com>
2024-07-11 06:44:53 -0300

    Remove unnecessary files

--- api/src/routes/users.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createUser, getUsers } from "../controllers/userController";
-
-const router = Router();
-
-router.post("/", createUser);
-router.get("/", getUsers);
-
-export { router as userRouter };
